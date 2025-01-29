38119a8f3e20abd94ff9fa33f0820b3e72fee620
ezequiel stom <ezequielstom@gmail.com>
2025-01-29 20:57:46 -0300

    Agregar nuevos proyectos de teatro y mejorar el diseño del encabezado y la galería

--- src/components/SimpleGallery/SimpleGallery.jsx
+++ src/components/SimpleGallery/SimpleGallery.jsx
@@ -19,7 +19,7 @@ export function SimpleGallery(props) {
 
   return (
     <div
-      className="pswp-gallery sticky top-0 z-10 grid h-full grid-cols-2 flex-wrap  items-start justify-center gap-2 overflow-x-auto p-4 sm:flex lg:px-4"
+      className="pswp-gallery sticky top-0 z-10 grid h-full grid-cols-2 flex-wrap  items-start justify-center gap-2 overflow-x-auto p-4 sm:flex lg:px-4 w-full"
       id={props.galleryID}
     >
       {props.images.map((image, index) => (
